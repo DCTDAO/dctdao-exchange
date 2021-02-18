2478a20b7ae78616aba7e7fbb809b7db6ea562fa
DCTDAO <info@dctdao.org>
2021-02-18 13:02:13 +0100

    Updating naming

--- src/data/Reserves.ts
+++ src/data/Reserves.ts
@@ -35,7 +35,6 @@ export function usePairs(currencies: [Currency | undefined, Currency | undefined
       }),
     [tokens]
   )
-  console.log("PairAddresses: ", pairAddresses)
 
   const results = useMultipleContractSingleData(pairAddresses, PAIR_INTERFACE, 'getReserves')
 
